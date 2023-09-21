2ef1b9cb40ee144b2866c1c30a8a2091bc5ed16f
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-09-21 18:26:55 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,29 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
 
 const App = () => {
-  const totalFruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
-  let [fruits,setfruits] = useState(totalFruits);
-  console.log(fruits);
+  const [fruitItem, setFruitItem] = useState(fruits);
 
-  const showSuggestion= (e)=>{
-    let filteredFruits = [];
-    filteredFruits = totalFruits.filter((value)=>{
-      return value.toLowerCase().includes(e.target.value);
-    })
-    setfruits(filteredFruits);
+  function handleSearchInput(e) {
+    let filteredFruit = fruits.filter(
+      (fruit) => fruit.indexOf(e.target.value) > -1
+    );
+    setFruitItem(filteredFruit);
   }
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        {/* <h1>Hello...</h1>b */}
-        <h1>Search item</h1>
-        <input type="text" onChange={(e)=>showSuggestion(e)}/>
-        <ul>
-          {      
-            fruits.map((value,index)=>(
-              <li key={index}>{value}</li>
-            ))
-          }
-        </ul>
+      {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onChange={handleSearchInput} />
+      <ul>
+        {fruitItem.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
